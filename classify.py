9a1549582c4e016ec300b5129b2b34827d67531d
jiajinliang <jil904@ucsd.edu>
2019-06-02 15:43:59 -0700

    good examples added

--- classify.py
+++ classify.py
@@ -15,8 +15,14 @@ def evaluate(X, yt, cls, name='data', verbose = True):
     """Evaluated a classifier on the given labeled data using accuracy."""
     from sklearn import metrics
     yp = cls.predict(X)
+    predict_prob = cls.predict_proba(X)
+    for i in range(len(yt)):
+            if yp[i] != yt[i] and predict_prob[i][0]>0.90:
+                print(i+1)
+#     print(predict_prob)
     acc = metrics.accuracy_score(yt, yp)
     if verbose:
         print("  Accuracy on %s is: %s" % (name, acc))
+        
     return acc
     
